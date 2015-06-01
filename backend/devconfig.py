7e44f470b202f0e930202946e26ddfd7185398b3
Daniel Que <dmdque@gmail.com>
2015-06-01 13:45:15 -0400

    added dbconfig files

---
+++ backend/devconfig.py
@@ -0,0 +1,5 @@
+DATABASE = '/tmp/deptimes.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
---
+++ backend/prodconfig.py
@@ -0,0 +1,5 @@
+DATABASE = '/app/backend/deptimes.northern-cali.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
